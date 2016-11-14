86be249b80cce319a86f383e6037722ad53d57a8
윤희상 <stu@DESKTOP-19LG7R5>
2016-11-14 14:29:19 +0900

    asdasd

--- SampleJava/src/LDG.java
+++ SampleJava/src/LDG.java
@@ -5,5 +5,7 @@ public class LDG {
 		System.out.println("올ㅋ 올라감");
 		System.out.println("이런 내맘 모르고 너무해!!");
 		System.out.println("올ㅋ 또 올라감");
+		System.out.println("올ㅋ 또 올라감");
+		System.out.println("올ㅋ 또 올라감");
 	}
 }
