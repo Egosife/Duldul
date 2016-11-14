ca7b459574e38eecdac1c776facffb5076d14d87
student <student@DESKTOP-MHENQ36>
2016-11-14 14:40:47 +0900

    업로드~

--- SampleJava/src/LDG.java
+++ SampleJava/src/LDG.java
@@ -6,6 +6,6 @@ public class LDG {
 		System.out.println("이런 내맘 모르고 너무해!!");
 		System.out.println("올ㅋ 또 올라감");
 		System.out.println("올ㅋ 또 올라감");
-		System.out.println("올ㅋ 또 올라감");
+		System.out.println("트사랑 TT");
 	}
 }
